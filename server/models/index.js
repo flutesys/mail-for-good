a59536f4ea5c66e1401bc1db23a05119717f887d
Isa Hassen <hassen.isa@gmail.com>
2017-10-20 11:44:59 -0400

    Revert "heroku procfile settings"
    This reverts commit 4cc7612b78a3514e7d05a0f16b90cc2421d35bae.

--- server/models/index.js
+++ server/models/index.js
@@ -7,8 +7,6 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/sequelize_config.js')[env];
 var db        = {};
-var pg = require('pg');
-pg.defaults.ssl = true;
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
